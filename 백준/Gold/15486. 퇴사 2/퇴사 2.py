1115e3f78d26feafa6e58d004e9f95197881ca41
eclipse25 <109349939+eclipse25@users.noreply.github.com>
2024-02-05 00:47:47 +0900

    [Gold V] Title: 퇴사 2, Time: 4348 ms, Memory: 536544 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,19 @@
+import sys
+n = int(sys.stdin.readline())
+end_day_dict = {}
+
+for i in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    end_day = i + a
+    if end_day in end_day_dict:
+        end_day_dict[end_day].append((i, b))
+    else:
+        end_day_dict[end_day] = [(i, b)]
+
+dp = [0] * (n + 1)
+for i in range(1, n + 1):
+    dp[i] = dp[i - 1]
+    if i in end_day_dict:
+        for j, point in end_day_dict[i]:
+            dp[i] = max(dp[i], dp[j] + point)
+print(dp[n])
