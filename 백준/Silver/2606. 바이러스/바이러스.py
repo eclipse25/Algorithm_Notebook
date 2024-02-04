ab258e3738566bfb3b231d6186b739135d8af5eb
eclipse25 <109349939+eclipse25@users.noreply.github.com>
2024-02-05 00:49:31 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,28 @@
+import sys
+
+n = int(sys.stdin.readline())
+p = int(sys.stdin.readline())
+graph = [[] for _ in range(n + 1)]
+
+for i in range(p):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+i = []
+visited = [False] * (n + 1)
+
+def dfs(graph, start, visited):
+    visited[start] = True
+    for i in graph[start]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+dfs(graph, 1, visited)
+
+count = 0
+for i in range(2, len(visited)):
+    if visited[i] == True:
+        count += 1
+
+print(count)
