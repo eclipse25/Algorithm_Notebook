af2d552db84d9593e8863d0b26cf2d28c22baa5c
eclipse25 <109349939+eclipse25@users.noreply.github.com>
2024-02-05 00:48:35 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+sq = []
+
+def backtracking(start):
+  if len(sq) == m:
+    print(' '.join(map(str, sq)))
+          
+  for i in range(start, n + 1):
+    if i not in sq :
+      sq.append(i)
+      backtracking(i + 1)
+      sq.pop()
+    
+backtracking(1)
