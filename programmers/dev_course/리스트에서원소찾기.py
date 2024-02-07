32d8b6e5a191a838404f0c4ae4cfee499610bb6b
eclipse25 <109349939+eclipse25@users.noreply.github.com>
2024-02-07 13:32:30 +0900

    Delete programmers directory

--- programmers/dev_course/리스트에서원소찾기.py
+++
@@ -1,8 +0,0 @@
-def solution(L, x):
-    answer = []
-    for i in range(len(L)):
-        if x == L[i]:
-            answer.append(i)
-    if len(answer) == 0:
-        answer = [-1]
-    return answer
--- programmers/dev_course/피보나치순열.py
+++
@@ -1,9 +0,0 @@
-def solution(x):
-    answer = 0
-    def fib(n):
-        if n <= 1:
-            return n
-        else:
-            return fib(n-2) + fib(n-1)
-    answer = fib(x)
-    return answer
