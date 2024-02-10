ddaf94ca6a5ccf2ed9e6f1aa4d66bbb3e9027f34
eclipse25 <109349939+eclipse25@users.noreply.github.com>
2024-02-11 01:34:43 +0900

    [Silver II] Title: 유기농 배추, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,39 @@
+import sys
+
+def dfs(x, y, field, visited):
+  # 스택 초기화 및 시작 노드 추가
+  stack = [(x, y)]
+  while stack:
+    (x, y) = stack.pop()
+    # 현재 위치를 방문 처리
+    if visited[y][x] == False:
+      visited[y][x] = True
+      # 상하좌우 네 방향에 대해 탐색
+      for (dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        nx, ny = x + dx, y + dy
+        # 다음 위치가 필드 안에 있고, 방문하지 않았으며, 배추가 심어져 있는지 확인
+        if 0 <= nx < len(field[0]) and 0 <= ny < len(field) and not visited[ny][nx] and field[ny][nx] == 1:
+          stack.append((nx, ny))
+
+
+t = int(sys.stdin.readline())  # 테스트 케이스의 수
+for _ in range(t):
+  m, n, k = map(int, sys.stdin.readline().split())  # 배추밭의 가로길이 m, 세로길이 n, 배추 위치의 개수 k
+  field = [[0] * m for _ in range(n)]  # 배추밭 초기화
+  visited = [[False] * m for _ in range(n)]  # 방문 여부 체크를 위한 배열 초기화
+
+  # 배추 위치 입력 받기
+  for _ in range(k):
+    x, y = map(int,sys.stdin.readline().split())
+    field[y][x] = 1  # 배추 위치를 1로 표시
+
+  # 필요한 지렁이 수 초기화
+  worms = 0
+
+  for i in range(n):
+    for j in range(m):
+      if field[i][j] == 1 and not visited[i][j]:
+        dfs(j, i, field,visited)
+        worms += 1
+
+  print(worms)
