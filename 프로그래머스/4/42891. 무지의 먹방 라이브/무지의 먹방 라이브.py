275dfbc187cb277044e4766fecc8eb2216d1b626
yunseojin <109349939+eclipse25@users.noreply.github.com>
2024-03-27 18:03:14 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 207.93 ms, Memory: 43.5 MB -BaekjoonHub

---
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
@@ -0,0 +1,28 @@
+import heapq
+
+def solution(food_times, k):
+    # 모든 음식을 먹는데 필요한 시간이 k보다 작거나 같으면 더 이상 먹을 음식이 없으므로 -1 반환
+    if sum(food_times) <= k:
+        return -1
+
+    # 음식 시간과 인덱스를 튜플로 묶어 최소 힙에 추가
+    food_heap = [(time, i) for i, time in enumerate(food_times, start=1)]
+    heapq.heapify(food_heap)
+
+    total_time = 0  # 누적된 시간
+    prev_time = 0  # 직전에 먹은 음식 시간
+    while food_heap:
+        # 현재 음식을 모두 먹는데 걸리는 시간
+        curr_time = (food_heap[0][0] - prev_time) * len(food_heap)
+        if total_time + curr_time > k:
+            # 남은 시간 내에 모든 음식을 먹을 수 없는 경우
+            # 인덱스 순으로 남은 음식 정렬
+            index_sorted_foods = sorted(food_heap, key=lambda x: x[1])
+            # k초 후에 먹어야 하는 음식의 인덱스를 반환
+            return index_sorted_foods[(k - total_time) % len(food_heap)][1]
+        # 현재 음식을 모두 먹고 다음 음식으로 넘어감
+        total_time += curr_time
+        prev_time, _ = heapq.heappop(food_heap)
+        
+    return -1
+        
