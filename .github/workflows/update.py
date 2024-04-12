ef8f0631c1c42511adbed5aa4b868e4e4174276c
yunseojin <109349939+eclipse25@users.noreply.github.com>
2024-04-13 01:56:15 +0900

    Update update.py

--- .github/workflows/update.py
+++ .github/workflows/update.py
@@ -50,8 +50,7 @@ def main():
 
         for file in files:
             if category not in solveds:
-                content += "|{}|[링크]({})|\n".format(category,
-                                                    parse.quote(os.path.join(root, file)))
+                content += "|{}|[링크]({})|\n".format(category,parse.quote(os.path.join(root, file)))
                 solveds.append(category)
                 print("category : " + category)
 
