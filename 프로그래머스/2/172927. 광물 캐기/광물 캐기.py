7285fb755d9bdcacd17fdb3858abd048f4823be0
Seojin Yun <109349939+eclipse25@users.noreply.github.com>
2025-03-21 23:31:02 +0900

    [level 2] Title: 광물 캐기, Time: 164.27 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -5,26 +5,28 @@ def backtrack(picksd, minerals, exh):
     if sum(picksd.values()) == 0 or not minerals:
         results.append(exh)
         return
-
+    
     for key in picksd.keys():
         if picksd[key] > 0:
-            new_picksd = picksd.copy()
-            new_picksd[key] -= 1
-
-            next_minerals = minerals[:5]
+            picksd[key] -= 1
+            next_minerals = minerals[0:5]
             new_exh = 0
-
             for mineral in next_minerals:
                 if ranks[key] <= ranks[mineral]:
                     new_exh += 1
                 else:
                     new_exh += 5 ** (ranks[key] - ranks[mineral])
-
-            backtrack(new_picksd, minerals[5:], exh + new_exh)
+            backtrack(picksd, minerals[5:], exh + new_exh)
+            picksd[key] += 1
+        
+                
+        
+        
 
 def solution(picks, minerals):
-
+    
     picksd = {"diamond": picks[0], "iron": picks[1], "stone": picks[2]}
+    
     backtrack(picksd, minerals, 0)
-
+    
     return min(results)
