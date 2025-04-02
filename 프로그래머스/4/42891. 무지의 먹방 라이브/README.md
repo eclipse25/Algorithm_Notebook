a84e87ed35274c2c8c892363a6d4cff90925d90c
Seojin Yun <109349939+eclipse25@users.noreply.github.com>
2025-04-03 00:22:51 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 306.89 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/4/42891. 무지의 먹방 라이브/README.md
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/README.md
@@ -1,10 +1,10 @@
 # [level 4] 무지의 먹방 라이브 - 42891 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42891#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42891) 
 
 ### 성능 요약
 
-메모리: 43.5 MB, 시간: 207.93 ms
+메모리: 38.3 MB, 시간: 306.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 27일 18:03:12
+2025년 04월 03일 00:22:48
 
 ### 문제 설명
 
--- 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
@@ -10,19 +10,19 @@ def solution(food_times, k):
     heapq.heapify(food_heap)
 
     total_time = 0  # 누적된 시간
-    prev_time = 0  # 직전에 먹은 음식 시간
+    prev_time = 0  # while문의 직전 시행에서 음식을 먹는데 걸린 시간
     while food_heap:
-        # 현재 음식을 모두 먹는데 걸리는 시간
+        # 가장 적게 남은 음식을 모두 먹는데 걸리는 시간(그동안 다른 음식들도 같은 양만큼 먹음)
         curr_time = (food_heap[0][0] - prev_time) * len(food_heap)
-        if total_time + curr_time > k:
-            # 남은 시간 내에 모든 음식을 먹을 수 없는 경우
+        if total_time + curr_time > k: 
+            # 이번 사이클(모든 음식에서 동일한 시간만큼 먹는 것)을 끝내기 전에 k초가 도달한 경우
             # 인덱스 순으로 남은 음식 정렬
             index_sorted_foods = sorted(food_heap, key=lambda x: x[1])
             # k초 후에 먹어야 하는 음식의 인덱스를 반환
             return index_sorted_foods[(k - total_time) % len(food_heap)][1]
         # 현재 음식을 모두 먹고 다음 음식으로 넘어감
         total_time += curr_time
-        prev_time, _ = heapq.heappop(food_heap)
+        prev_time, _ = heapq.heappop(food_heap) # 각 음식이 지금까지 먹힌 횟수
         
     return -1
         
