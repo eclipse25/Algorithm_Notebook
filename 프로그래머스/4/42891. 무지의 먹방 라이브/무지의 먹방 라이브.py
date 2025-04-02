9336e6fc50592bc2d0eb1e7a0bb274bfd803a389
Seojin Yun <109349939+eclipse25@users.noreply.github.com>
2025-04-03 00:26:16 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 294.36 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
@@ -10,7 +10,7 @@ def solution(food_times, k):
     heapq.heapify(food_heap)
 
     total_time = 0  # 누적된 시간
-    prev_time = 0  # while문의 직전 시행에서 음식을 먹는데 걸린 시간
+    prev_time = 0
     while food_heap:
         # 가장 적게 남은 음식을 모두 먹는데 걸리는 시간(그동안 다른 음식들도 같은 양만큼 먹음)
         curr_time = (food_heap[0][0] - prev_time) * len(food_heap)
