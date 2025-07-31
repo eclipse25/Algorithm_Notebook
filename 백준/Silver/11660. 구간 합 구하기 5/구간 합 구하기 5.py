98d653124385445605ea57bf70e2503d00728f11
Seojin Yun <109349939+eclipse25@users.noreply.github.com>
2025-07-31 11:38:20 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1048 ms, Memory: 106824 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -5,20 +5,14 @@ n, m = map(int, sys.stdin.readline().split())
 for _ in range(n):
     table.append(list(map(int, sys.stdin.readline().split())))
 
-p_sum = [[0] * (n + 1) for _ in range(n + 1)] 
-
+dpt = [[0] * (n + 1) for _ in range(n + 1)]
 for i in range(1, n + 1):
     for j in range(1, n + 1):
-        p_sum[i][j] = table[i - 1][j - 1] + p_sum[i - 1][j] + p_sum[i][j - 1] - p_sum[i - 1][j - 1]
+        dpt[i][j] = (
+            dpt[i - 1][j] + dpt[i][j - 1] - dpt[i - 1][j - 1] + table[i - 1][j - 1]
+        )
 
 for _ in range(m):
     x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
-    answer = p_sum[x2][y2]
-    if x1 > 1:
-        answer -= p_sum[x1 - 1][y2]
-    if y1 > 1:
-        answer -= p_sum[x2][y1 - 1]
-    if x1 > 1 and y1 > 1:
-        answer += p_sum[x1 - 1][y1 - 1]
-
+    answer = dpt[x2][y2] - dpt[x1 - 1][y2] - dpt[x2][y1 - 1] + dpt[x1 - 1][y1 - 1]
     print(answer)
